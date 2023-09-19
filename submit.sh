83fa6199151bcbd8f4a2e36301773fe29db3bc4d
Nick Van Alfen <nvanalfen2@gmail.com>
2023-09-19 13:02:36 -0400

    Add submit script for slurm

---
+++ submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J ia_ml
+#SBATCH --partition=short
+#SBATCH --time=22:00:00
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --array=1-50%50
+#SBATCH --output=output_logs/%A-%a.out
+#SBATCH --error=error_logs/%A-%a.err
+#SBATCH --mail-user=nvanalfen2@gmail.com
+#SBATCH --mail-type=ALL
+
+#module load discovery anaconda3/3.7
+source /home/nvanalfen/miniconda3/bin/activate
+conda activate ia_ml
+
+python generate_training_data.py test_bolplanck.npy $SLURM_ARRAY_TASK_ID 50
+
