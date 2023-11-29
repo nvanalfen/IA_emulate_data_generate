7c4b7f93e2be578bf8d90d9ead51e81a0aee4033
Nick Van Alfen <nvanalfen2@gmail.com>
2023-11-29 14:54:05 -0500

    Add somewhat realistic parameter traversal

---
+++ submit_realistic.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J ia_ml
+#SBATCH --partition=short
+#SBATCH --time=22:00:00
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --array=1-50%50
+#SBATCH --output=output_logs/%A-%a.out
+#SBATCH --error=error_logs/%A-%a.err
+#SBATCH --mail-user=nvanalfen2@gmail.com
+#SBATCH --mail-type=ALL
+
+#module load discovery anaconda3/3.7
+source /home/nvanalfen/miniconda3/bin/activate
+conda activate ia_ml
+JOB=$(($SLURM_ARRAY_TASK_ID+0))
+
+python generate_training_data_realistic.py $JOB 50 150000
